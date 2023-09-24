a56d4d3fe864dc9d7af703c1edc40e37d84f56b4
Hemanth Kumar <85030810+HemanthKumar8251@users.noreply.github.com>
2023-09-24 21:55:28 +0530

    Added Comments

--- EJS Partials/index.js
+++ EJS Partials/index.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const app = express();
-const port = 3000;
+const port = 9999;
 
 /* Write your code here:
 Step 1: Render the home page "/" index.ejs
